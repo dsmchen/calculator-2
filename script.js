08ce1b7d11d71455559add98eabfbdc6d71fb7e5
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-08-20 14:37:12 +0800

    Add function for long expressions

--- script.js
+++ script.js
@@ -42,19 +42,31 @@ function handleDisplay(e) {
   if (display.textContent === '0' && e.target.classList.contains('digit')) {
     display.textContent = '';
   }
-  display.textContent += e.target.value;
+  const operatorRegex = /\D/;
+  if (
+    operatorRegex.test(display.textContent) &&
+    e.target.classList.contains('operator')
+  ) {
+    handleEquals(e.target.value);
+  } else {
+    display.textContent += e.target.value;
+  }
 }
 const displayCharacter = document.querySelectorAll('.display-character');
 displayCharacter.forEach((char) =>
   char.addEventListener('click', handleDisplay)
 );
 
-function handleEquals() {
+function handleEquals(lastOperator = 0) {
   let expression = display.textContent;
   let numbers = expression.match(/\d+/g);
   numberA = Number(numbers[0]);
   numberB = Number(numbers[1]);
   operator = expression.match(/\D/g)[0];
   operate(operator, numberA, numberB);
+
+  if (lastOperator && typeof lastOperator !== 'object') {
+    display.textContent += lastOperator;
+  }
 }
 equals.addEventListener('click', handleEquals);
